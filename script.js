f8de421aa2e8f7b437aeac8dbcd14a65650db5e6
laurabramani <115945225+laurabramani@users.noreply.github.com>
2023-02-22 11:51:40 +0000

    i change the menu

--- script.js
+++
@@ -1,8 +0,0 @@
-// Get references to the nav button and curtain
-const navBtn = document.querySelector('.nav-btn');
-const navCurtain = document.querySelector('.nav-curtain');
-
-// Toggle the 'open' class on the curtain when the nav button is clicked
-navBtn.addEventListener('click', function() {
-  navCurtain.classList.toggle('open');
-});
