cbe5a5b2c97e60ed865a02db735b977d38c2f919
laurabramani <115945225+laurabramani@users.noreply.github.com>
2023-05-10 10:19:56 +0000

    cambio pagina con script per banner cookie policy

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+//cookie policy
+let acc = document.querySelector(".span-acc");
+let noAcc = document.querySelector(".span-non-acc");
+let divCookie = document.querySelector(".div-cookie");
+let n = 0;
+
+noAcc.addEventListener('click', function() {
+    divCookie.style.display = "none";
+    console.log(n++);
+});
+
+acc.addEventListener('click', function() {
+    divCookie.style.display = "none";
+    console.log(n++);
+});
